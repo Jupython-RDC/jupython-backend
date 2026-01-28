7cb769f4a9e1e4b6dc9d384ec8fceb5fe2fd4676
MvExauce <MvExauce@github.com>
2026-01-28 10:17:16 +0100

    Backend ready: Django + PostgreSQL configured, APIs operational

--- jupython/settings.py
+++ jupython/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'jupython.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jupython_db',
+        'USER': 'postgres',
+        'PASSWORD': 'codepro',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
